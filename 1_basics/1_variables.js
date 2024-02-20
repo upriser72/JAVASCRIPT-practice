274c0efbd92ea5ddb2949db6bec7814f3b2eeed2
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-02-20 08:30:35 +0000

    variables and constants

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,13 @@
+const accountId=14635
+let accountEmail="vanshika@gmail.com"
+var accountPw="12345"
+accountCity="Kanpur"
+let accountState
+
+// accountId=554 //not allowed to change constant
+accountEmail="vs@gmail.com"
+accountPw="527347"
+accountCity="Lucknow"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPw,accountCity,accountState])
