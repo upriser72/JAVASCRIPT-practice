e285ae7fdfb45ec471d2ce1721477ccd0140d9cc
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-02-21 18:19:43 +0000

    .

--- 1_basics/3_conversionOperation.js
+++ 1_basics/3_conversionOperation.js
@@ -4,3 +4,7 @@ console.log(typeof(age))
 
 let score="26"
 console.log(typeof(score))
+
+let valueInNumber = Number(score)     //conversion of string to number 
+                                      // use capital Number not number
+console.log(typeof(valueInNumber))
