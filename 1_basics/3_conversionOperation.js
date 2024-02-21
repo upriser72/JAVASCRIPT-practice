1f5fda5565b4ebf089872e6a621a567bf4a98e93
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-02-21 18:54:27 +0000

    operations on js

--- 1_basics/3_conversionOperation.js
+++ 1_basics/3_conversionOperation.js
@@ -22,4 +22,17 @@ console.log(nextInNumber)      //returns/prints 0
 conversion of undefined to number will give NaN
 conversion of boolean true to number will give 1
 conversion of boolean false to number wioll give 0
+conversion of string like "vanshika" to number will give NaN
 */
+
+let currState=1
+let changedToBoolean =Boolean(currState)
+console.log(changedToBoolean)      //number 1 got converted to boolean true
+
+let newState=" "
+let ToBoolean =Boolean(newState)
+console.log(ToBoolean)   //non empty string converted to boolean true
+
+let newState1=""
+let ToBoolean1 =Boolean(newState1)
+console.log(ToBoolean1)  //empty string converted to boolean false
---
+++ 1_basics/4_operations.js
@@ -0,0 +1,27 @@
+let value =7
+let negValue= -value
+console.log(negValue) //returns negative of the given number
+
+/*
+various operations:
+console.log(2+2)
+           (2-2)
+           (2*2)
+           (2**3) =>gives 2 raised to the power 3
+           (2/3)
+           (2%3) =>gives modulus 
+*/
+
+//string concatenation
+let str1 ="hello"
+let str2=" vanshika"
+let str3= str1 + str2
+console.log(str3);
+
+//some confusing cases:
+console.log("1" + 2); //12
+console.log( 1 + "2") //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2") //32
+console.log(1 + 2) //3
+console.log("1" + "2") //12
