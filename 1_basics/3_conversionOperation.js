9bb6a5f4b06962685f3372c292918d4315ec0730
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-02-21 18:25:48 +0000

    .

--- 1_basics/3_conversionOperation.js
+++ 1_basics/3_conversionOperation.js
@@ -8,3 +8,12 @@ console.log(typeof(score))
 let valueInNumber = Number(score)     //conversion of string to number 
                                       // use capital Number not number
 console.log(typeof(valueInNumber))
+
+let newscore="26abc"     //have both number and chars
+let newValueInNumber =Number(newscore)
+console.log(typeof(newValueInNumber))   //returns number
+console.log(newValueInNumber)   //returns NaN which means n26abc got converted to a number but not valid conversion
+
+let next =null
+let nextInNumber =Number(next)
+console.log(nextInNumber)      //returns/prints 0
