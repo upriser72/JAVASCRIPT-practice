31089c71f8da33b45f338ef3a4c6e28eede2b0ba
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-02-22 17:56:15 +0000

    summary

---
+++ 1_basics/6_datatypes-summary.js
@@ -0,0 +1,44 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3 //Number is not int float or double every number is number
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; //undefined datatype
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["happy","henry","likes","beans"];
+let myObj = {
+    name: "vanshika",
+    age: 23,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+console.log(typeof bigNumber);
+console.log(typeof myFunction);
+console.log(typeof userEmail);
+console.log(typeof heros);
+console.log (typeof myObj);
+
+//function is called function object other than that, object and array is called object
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
