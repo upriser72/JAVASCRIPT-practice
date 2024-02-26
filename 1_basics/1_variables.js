e1b7ac53907d4ffcfe6441ce3ad27a08b15ff139
vanshika8692316 <upriser72@gmail.com>
2024-02-26 16:16:00 +0530

    variable

--- 1_basics/1_variables.js
+++ 1_basics/1_variables.js
@@ -1,17 +1,17 @@
-const accountId=14635
-let accountEmail="vanshika@gmail.com"
-var accountPw="12345"
-accountCity="Kanpur"
-let accountState
+const accountId = 14635;
+let accountEmail = "vanshika@gmail.com";
+var accountPw = "12345";
+accountCity = "Kanpur";
+let accountState;
 
 // accountId=554 //not allowed to change constant
-accountEmail="vs@gmail.com"
-accountPw="527347"
-accountCity="Lucknow"
+accountEmail = "vs@gmail.com";
+accountPw = "527347";
+accountCity = "Lucknow";
 
 /*
 Prefer not to use var because it has issues with block and functional scope
-*/
+*/ //
 
 console.log(accountId);
-console.table([accountId,accountEmail,accountPw,accountCity,accountState])
+console.table([accountId, accountEmail, accountPw, accountCity, accountState]);
