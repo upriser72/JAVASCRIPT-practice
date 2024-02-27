97a5be78fadcfb804d3851fe5b8a20f51eb0989a
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-02-27 19:36:49 +0000

    date

--- 1_basics/10_dates.js
+++ 1_basics/10_dates.js
@@ -1,3 +1,19 @@
 let mydate = new Date()
 console.log(mydate)
 console.log(mydate.toString())
+console.log(mydate.toDateString())
+console.log(mydate.toISOString())
+console.log(mydate.toJSON())
+console.log(mydate.toLocaleDateString())
+console.log(mydate.toLocaleString())
+console.log(mydate.toLocaleTimeString())
+
+console.log(typeof mydate) //object
+
+let myCreatedDate = new Date(2023, 0, 23) 
+ //MONTH STARTS FROM 0 IN JS THAT IS JANUARY IS 0 NOT 1
+
+//let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+//let myCreatedDate = new Date("2023-01-14")
+//let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
