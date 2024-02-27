ab3c8453c3d12d351e91fae7af193586ee901e84
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-02-27 17:53:30 +0000

    numbers

---
+++ 1_basics/9_numbers_and_maths.js
@@ -0,0 +1,16 @@
+const score= 100
+console.log(score)
+
+const balance = new Number(100)//using  object 
+console.log(balance) 
+/*
+  VARIOUS OPERATIONS ON NUMBER 
+constructor: ƒ Number()
+toExponential: ƒ toExponential()
+toFixed: ƒ toFixed()
+toLocaleString: ƒ toLocaleString()
+toPrecision: ƒ toPrecision()
+toString: ƒ toString()
+valueOf: ƒ valueOf()
+*/
+
