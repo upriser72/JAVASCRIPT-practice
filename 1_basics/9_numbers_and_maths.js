ededaa654eefefcdf776d86ff208d88cca88eee3
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-02-27 19:19:58 +0000

    maths

--- 1_basics/9_numbers_and_maths.js
+++ 1_basics/9_numbers_and_maths.js
@@ -34,3 +34,15 @@ console.log(Math) //Object [Math] {} Math is an object
 console.log(Math.abs(-3))//3
 console.log(Math.round(3.8))//4
 console.log(Math.round(3.2))//3
+console.log(Math.ceil(3.2))//4
+console.log(Math.floor(3.2))//3
+
+
+console.log(Math.random()); // GIVES VALUES BETWEEN 0 AND 1
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) //IMPORTANT
