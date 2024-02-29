f584fdd775a737c06651ad4bd5f71cf2fa509c41
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-02-29 19:39:21 +0000

    destructuring

---
+++ 2_basics_pt-2/5_objects.js
@@ -0,0 +1,33 @@
+//DESTRUCTURING OF OBJECTS
+
+const course = {
+    coursename: "js fun",
+    price: "999",
+    courseInstructor: "dumbledore"
+}
+
+// course.courseInstructor  //long syntax to use multiple times
+
+//const {courseInstructor} = course
+// console.log(courseInstructor);
+
+const {courseInstructor: instructor} = course //gave an easy name
+console.log(instructor);
+
+
+//JSON format as object
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",  
+//     "price": "free"
+// }
+
+
+//JSON format as array
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
+//use JSON FORMATTER for understanding JSON codes
