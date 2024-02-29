862dbfbb69ccaa73ebbeb46098ec1ee7bc6bc599
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-02-29 18:41:21 +0000

    objects

--- 2_basics_pt-2/3_objects.js
+++ 2_basics_pt-2/3_objects.js
@@ -6,15 +6,22 @@ If object is made by constructor then its always singleton.
 
 // object literals
 
-
+const mySym = Symbol("key1")   // defining a symbol datatype 
 //keys are taken as string by default
 const JsUser = {
     name: "apple",
     "full name": "kirti sinha",
-    [mySym]: "mykey1",
+    [mySym]: "mykey1",  // this is how a symbol is passed/defined in an object 
     age: 18,
     location: "Jaipur",
     email: "something@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
+
+
+// console.log(JsUser.email)  //this approach doesn't work for object keys declared as "full name"
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])  //better approach
+// console.log(JsUser[mySym])
+
