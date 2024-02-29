d2041acf690e65ed10a33da2424e90ba68ae3496
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-02-29 19:13:12 +0000

    object

--- 2_basics_pt-2/4_objects.js
+++ 2_basics_pt-2/4_objects.js
@@ -3,3 +3,19 @@
 const tinderUser = {}               // non singleton object
 console.log(tinderUser)
 
+//defining object inside an object (NESTING)
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);  /retrieving values of nested objects
+
+
+
+
