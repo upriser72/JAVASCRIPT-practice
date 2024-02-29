83bd2eae1c51e6c2e7710a0f108a1ab01f4475dc
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-02-29 19:25:04 +0000

    objects

--- 2_basics_pt-2/4_objects.js
+++ 2_basics_pt-2/4_objects.js
@@ -15,6 +15,47 @@ const regularUser = {
 }
 
 // console.log(regularUser.fullname.userfullname.firstname);  /retrieving values of nested objects
+console.log(regularUser.fullname?.userfullname.firstname);
+// using question mark to cherck if fullname exists
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }  //returns object inside object
+// const obj3 = Object.assign({}, obj1, obj2, obj4) 
+//takes empty {} object as target and fills values of all the other source objects in it
+//hence helps in combining various objects
+
+const obj3 = {...obj1, ...obj2} //using spread operator
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email   //index used for selecting object 
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));  //gives array of all keys in tinderUser
+// console.log(Object.values(tinderUser)); //gives array of all values in tinderUser
+// console.log(Object.entries(tinderUser));  //gives separate arrays of key and value
+                                             //[key1,val1] [key2,val2]......
+
+                                             
+
 
 
 
