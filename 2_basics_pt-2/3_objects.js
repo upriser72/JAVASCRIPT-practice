259fdd43ecf5b9088e114be8fa9b3380e11f5299
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-02-29 18:28:11 +0000

    object

--- 2_basics_pt-2/3_objects.js
+++ 2_basics_pt-2/3_objects.js
@@ -4,3 +4,17 @@ If object is made by literal then its never a singleton.
 If object is made by constructor then its always singleton.
 */
 
+// object literals
+
+
+//keys are taken as string by default
+const JsUser = {
+    name: "apple",
+    "full name": "kirti sinha",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "something@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
