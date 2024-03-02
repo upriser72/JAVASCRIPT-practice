53e1599f42d666a3f85d9befe3cde179c0e88793
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-03-03 00:00:41 +0530

    Update 10_dates.js

--- 1_basics/10_dates.js
+++ 1_basics/10_dates.js
@@ -1,6 +1,6 @@
 let mydate = new Date()
 console.log(mydate)
-console.log(mydate.toString())
+//console.log(mydate.toString())
 console.log(mydate.toDateString())
 console.log(mydate.toISOString())
 console.log(mydate.toJSON())
