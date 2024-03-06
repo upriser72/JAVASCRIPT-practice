432ddc84ddcf8d5d9447cbd077a5b7e539c9072e
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-03-06 17:01:41 +0000

    if

--- 3_control_flow/1_if_else.js
+++ 3_control_flow/1_if_else.js
@@ -3,6 +3,17 @@
 const isUserloggedIn = true
 if(isUserloggedIn)
 {
-
 }
-//operators comparison : ==,<=,>=,<,>,!=  
+
+const temperature = 41
+
+//  if ( temperature === 40 )
+//  {
+//     console.log("less than 50");
+//  } 
+// else 
+// {
+//     console.log("temperature is greater than 50");
+// }
+
+//operators comparison : ==,<=,>=,<,>,!=,===(checks datatype),!==
