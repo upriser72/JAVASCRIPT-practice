da848dc88cf98c8f7c98858f0fdd9aaa5d7f1762
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-03-06 17:05:32 +0000

    if

--- 3_control_flow/1_if_else.js
+++ 3_control_flow/1_if_else.js
@@ -26,3 +26,34 @@ const temperature = 41
 // }
 
 // console.log(`User power: ${power}`);    //power not defined because out of block scope
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");    //implicit 
+
+//NESTING IF STATEMENTS
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
