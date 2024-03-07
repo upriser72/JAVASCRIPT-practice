7676cf13b73b157d85fd49513099c677ebbe49be
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-03-07 18:19:39 +0000

    for

--- 4_loops/1_for.js
+++ 4_loops/1_for.js
@@ -11,17 +11,21 @@ for (let i = 0; i <= 10; i++) {
 
 // console.log(element);
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) 
+{
     //console.log(`Outer loop value: ${i}`);
-   for (let j = 1; j <= 10; j++) {
+   for (let j = 1; j <= 10; j++) 
+   {
     //console.log(`Inner loop value ${j} and inner loop ${i}`);
     //console.log(i + '*' + j + ' = ' + i*j );
    }
     
 }
+
 let myArray = ["flash", "batman", "superman"]
 //console.log(myArray.length);
-for (let index = 0; index < myArray.length; index++) {
+for (let index = 0; index < myArray.length; index++)
+ {
     const element = myArray[index];
     //console.log(element);
     
@@ -39,8 +43,10 @@ for (let index = 0; index < myArray.length; index++) {
     
 // }
 
-for (let index = 1; index <= 20; index++) {
-    if (index == 5) {
+for (let index = 1; index <= 20; index++) 
+{
+    if (index == 5)
+     {
         console.log(`Detected 5`);
         continue
     }
