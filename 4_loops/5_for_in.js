7028d665ce0dae1397a743772639df37309ae5e6
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-03-23 20:03:26 +0000

    for

--- 4_loops/5_for_in.js
+++ 4_loops/5_for_in.js
@@ -15,3 +15,11 @@ for (const key in myObject)
 
 //FOR IN LOOP ON ARRAY
 
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(key)     //prints 0 1 2 3....
+    //array is stored as an object having default key values as 0 1 2.......
+    //console.log(programming[key]);  //prints js rb py .....
+}
+
