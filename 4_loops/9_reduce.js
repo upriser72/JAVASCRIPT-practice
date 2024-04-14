a1720874c6744eadac41fce6fadabd30ecc4dd88
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-04-14 14:00:25 +0000

    reduce

--- 4_loops/9_reduce.js
+++ 4_loops/9_reduce.js
@@ -15,7 +15,35 @@ console.log(sumWithInitial);
 */
 const myNums = [1, 2, 3]
 
-const myTotal = myNums.reduce(function (acc, currval) {
-    console.log(`acc: ${acc} and currval: ${currval}`);
-    return acc + currval
-}, 2)
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 2)
+//console.log(myTotal)
+
+const myTotal=myNums.reduce( (acc,curr) => acc+curr,0)
+//console.log(myTotal)
+
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const bill=shoppingCart.reduce( (acc,item) => (acc+item.price),0)
+console.log(`Your Payable Amount is: ${bill}`)
