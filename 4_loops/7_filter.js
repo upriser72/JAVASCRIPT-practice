e6a869d030cb137b74b96dc6fb61997abb4016c4
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-04-14 13:03:35 +0000

    filter

---
+++ 4_loops/7_filter.js
@@ -0,0 +1,12 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// //putting items of the loop in a variable
+// const values = coding.forEach( (item) => {
+//     console.log(item)
+// })
+
+// console.log(values)   //forEach loop returns nothing 
+
+//Filter
+
+
