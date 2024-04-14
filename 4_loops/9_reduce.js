0d74e9a547080c7cb6c6d48ec1522cbbf4db2e9c
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-04-14 13:52:37 +0000

    filter map

---
+++ 4_loops/9_reduce.js
@@ -0,0 +1,21 @@
+/*
+//SYNTAX
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+
+console.log(sumWithInitial);
+// Expected output: 10
+
+*/
+const myNums = [1, 2, 3]
+
+const myTotal = myNums.reduce(function (acc, currval) {
+    console.log(`acc: ${acc} and currval: ${currval}`);
+    return acc + currval
+}, 2)
